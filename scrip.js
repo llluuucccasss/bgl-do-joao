db40caa8a7c17a890acb6605b47a5e3e87d42ad0
llluuucccasss <aolmadasl@gmail.com>
2024-05-15 15:03:48 -0300

    first commit

---
+++ scrip.js
@@ -0,0 +1,28 @@
+let numero = 0;
+
+function soma(){
+    numero += 1 
+    console.log(numero)
+
+    
+switch (numero) {
+    case 4:
+        console.log("para de me apertar")
+        break;
+
+    case 18:
+        console.log("SERIO PARA")
+        break;
+
+    case 30:
+        console.log("PARA PELO AMOR DE DEUS")
+        break;
+    
+    case 40:
+        console.log("eu desisto")
+        break;
+    default:
+        
+        break;
+}    
+}
